9994e2cb922d3ab49a3028bf41a241b50c46a49f
Stalder <serg_lavinov@inbox.ru>
2021-04-02 15:18:51 +0300

    Fixed main in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import * as dotenv from 'dotenv'
+import { Config } from './config'
 import { importFromFigma } from './importFromFigma'
 
-dotenv.config()
-
-importFromFigma({
-  figmaToken: process.env.FIGMA_TOKEN!,
-  projectId: process.env.DOCUMENT_ID!,
-  exportType: 'react',
-  outputDir: './generated',
-})
+export { importFromFigma, Config }
